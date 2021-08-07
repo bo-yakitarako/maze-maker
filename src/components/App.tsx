f109a37bce8f2656f7df54368e178c78922e5939
bo-yakitarako <bo-yakitarako@users.noreply.github.com>
2021-08-07 09:32:40 +0900

    適当コミット

---
+++ src/components/App.tsx
@@ -0,0 +1,5 @@
+const App: React.FC = () => {
+  return <>とりあえずやってみることが大事</>;
+};
+
+export { App };
---
+++ src/modules/app.ts
@@ -0,0 +1,13 @@
+/* eslint-disable no-param-reassign */
+
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const app = createSlice({
+  name: 'template',
+  initialState,
+  reducers: {},
+});
+
+export { app };
